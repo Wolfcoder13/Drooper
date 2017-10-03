cb4f51469772b89f9384db4868e64ab21b6264b1
Wolfcoder13 <Wolfninja13@gmail.com>
2017-10-03 20:25:46 +0000

    more code for 4x4

---
+++ Code/DrumButton.py
@@ -0,0 +1,17 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+class DrumButton(object):
+	"""This represents a button on our raspberry pi
+    """
+	
+	def __init__(self, path):
+        """Initalizes the button to have a specific sound.
+		path is a string that holds the path to the sound, such as "path/to/sound1.wav" """
+        self.sound = AudioSegment.from_file(path)
+		
+	def playSound(volume):
+		""" This will play the sound that is stored on this button
+		volume: is a variable that will control how loudly the sound will be played."""
+		converted_volume = *0.3(-100+volume)
+		play(song-converted_volume)) #song minus volume will decrese the songs volume by converted_volume desibels
--- Code/DrumpadService.py
+++ Code/DrumpadService.py
@@ -7,67 +7,72 @@ import pyaudio
 from pydub import AudioSegment
 from pydub.playback import play
 
+from DrumButton import DrumButton
 
+class DrumpadService(object):
+	#TODO: See if we should make this into a singleton class.
 
-#TODO: See if we should make this into a singleton class.
-
-# TODO: need to verify these are the right numbers for the pins.
-# Software SPI configuration:
-CLK  = 18
-MISO = 23
-MOSI = 24
-CS   = 25
-CS2  = 26	#check this pin
-mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
-#make sure this works as second input
-mcp2 = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS2, miso=MISO, mosi=MOSI)
-
-
-
-# Hardware SPI configuration:
-# SPI_PORT   = 0
-# SPI_DEVICE = 0
-# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-# TODO: initialize drum sounds
-def initalizeDrums ():
-	#perhaps keep sounds in array
+	def __init__(self):
+	# TODO: need to verify these are the right numbers for the pins.
+	# Software SPI configuration:
+	self.CLK  = 18
+	self.MISO = 23
+	self.MOSI = 24
+	self.CS   = 25
+	self.CS2  = 26	#check this pin
+	self.mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+	#make sure this works as second input
+	self.mcp2 = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS2, miso=MISO, mosi=MOSI)
 	
-def changeDrums (pick):
-	#load other drums, pick will chose frome selectable drums
+	self.listOfDrums = []
 
-def playSound (button_nr, volume):
-	converted_volume = *0.2(-100+volume)
-	play(song-converted_volume)) #song minus volume will decrese the songs volume by converted_volume desibels
+	# Hardware SPI configuration:
+	# SPI_PORT   = 0
+	# SPI_DEVICE = 0
+	# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-def getVolume (input):
-	#calculates the volume from 10 bit input resolution (1023 values)
-	if input < 600:
-		return 0
-	else if input < 700:
-		return 20
-	else if input < 800:
-		return 40
-	else if input < 900:
-		return 60
-	else if input < 1000:
-		return 80
-	return 100
-	
-	
+	# TODO: initialize drum sounds
+	def initalizeDrums (self):
+		#perhaps keep sounds in array
+		for i in range(16):
+			self.listOfDrums[i] = DrumButton("/sound"+i+".wav")
+		
+		
+	def changeDrums (self,pick):
+		#load other drums, pick will chose frome selectable drums
 
-# TODO: see if its possible to use interrupt, instead of constant polling of 
-# Drumpad loop. Should be run asynchronously
-def prototype:
-	while True:
-		for i in range(8):
-			# The read_adc function will get the value of the specified channel (0-7).
-			volume = getVolume(mcp.read_adc(i))
-			volume2 = getVolume(mcp2.read_adc(i))
-			if volume != 0:
-				playSound(i, volume)
-			if volume2 != 0:
-				playSound(i+8, volume2)
+	def playButton (self,buttonNr, volume):
+		self.listOfDrums[buttonNr].playSound(volume)
 
-		# Pause for half a second.
-		time.sleep(0.01)
+	def getVolume (self,input):
+		#calculates the volume from 10 bit input resolution (1023 values)
+		#perhaps add more resolution.
+		if input < 600:
+			return 0
+		else if input < 700:
+			return 20
+		else if input < 800:
+			return 40
+		else if input < 900:
+			return 60
+		else if input < 1000:
+			return 80
+		return 100
+		
+		
+
+	# TODO: see if its possible to use interrupt, instead of constant polling of 
+	# Drumpad loop. Should be run asynchronously
+	def prototype(self):
+		while True:
+			for i in range(8):
+				# The read_adc function will get the value of the specified channel (0-7).
+				volume = getVolume(mcp.read_adc(i))
+				volume2 = getVolume(mcp2.read_adc(i))
+				if volume != 0:
+					playButton(i, volume)
+				if volume2 != 0:
+					playButton(i+8, volume2)
+
+			# Pause for half a second.
+			time.sleep(0.01)
